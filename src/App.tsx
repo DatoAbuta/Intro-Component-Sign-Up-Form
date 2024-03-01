d14ddd68bd85c87b789f9b7b6e232e5953b96bd8
David69699 <abutidzedato18@gmail.com>
2024-03-01 13:24:41 +0400

    update

--- src/App.tsx
+++ src/App.tsx
@@ -11,8 +11,8 @@ type SubmitType = {
 }
 
 const schema = yup.object().shape({
-  name: yup.string().required('First Name is necessary'),
-  lastname: yup.string().required("Last Name cannot be empty"),
+  name: yup.string().required('First Name is necessary').matches(/^[a-zA-Z]+$/, 'Only letters are allowed'),
+  lastname: yup.string().required("Last Name cannot be empty").matches(/^[a-zA-Z]+$/, 'Only letters are allowed'),
   email: yup.string().required("Looks like this is not an email").email("Email Isn't Valid"),
   password: yup.string().required("Password cannot be empty").min(6, "Minimum 6 Symbol").max(20, "Too many")
 })
